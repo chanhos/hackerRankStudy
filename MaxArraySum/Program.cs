753f6cc1ebaa8c83c0f41f2da7f8ab1311f8a411
찬호 정 <lovgone@naver.com>
2020-11-27 18:35:39 +0900

    commit2020/1127

--- MaxArraySum/Program.cs
+++ MaxArraySum/Program.cs
@@ -9,7 +9,23 @@ namespace MaxArraySum
     {
         static void Main(string[] args)
         {
+            int[] rod = new int[] { 0, 1, 5, 8, 9, 10, 17, 20, 24, 30 }; 
 
+        }
+
+
+        static int CutRod(int[] p , int n)
+        {
+
+            for (int j = 0; j < n; j++)
+            {
+                int q = -1;
+                for (int i = 1; i < j; j++)
+                {
+
+                }
+            }
+            return 0;
         }
     }
 }
